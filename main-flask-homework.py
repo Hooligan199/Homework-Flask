bf08b0b215f045b7ab61e89599a682215287fc1f
Pustelnyk Davyd <forrestgamp1994@gmail.com>
2022-06-16 02:12:07 +0300

    working project (I guess)

--- main-flask-homework.py
+++ main-flask-homework.py
@@ -1,7 +1,7 @@
 import random, string
 import pandas as pd
-from flask import Flask
-from markupsafe import escape
+import requests
+from flask import Flask, request
 from faker import Faker
 
 app = Flask(__name__)
@@ -20,9 +20,9 @@ def greeting():
     return text
 
 @app.route("/generate-users/")
-@app.route("/generate-users/<amount>")
-def generator(amount):
+def generator():
     double_string = ""
+    amount = request.args.get('count', default = 100, type = int)
     for _ in range(int(amount)):
         double_string += f"<p>{fake.name()} " \
                          f"{''.join(random.choice(string.ascii_lowercase + string.digits) for _ in range(9))}" \
@@ -33,10 +33,26 @@ def generator(amount):
 @app.route("/mean/")
 def calculated_csv():
     data = pd.read_csv("hw.csv")
-    print(data.columns)
+    height_weight = pd.DataFrame(
+        data, columns=['Height(Inches)', 'Weight(Pounds)']).values
+    total_canti = 0
+    total_kilo = 0
+    for canti, kilo in height_weight:
+        total_canti += canti
+        total_kilo += kilo
+    return f"<p>Средний вес = {round(total_kilo * 0.454 / 25000)}кг</p>" \
+           f"<p>Средний рост(в см) = {round(total_canti * 2.54 / 25000)}см</p>"
+
+@app.route("/space/")
+def cosmonauts_online():
+    try:
+        answer = requests.get("http://api.open-notify.org/astros.json")
+    except ConnectionError:
+        return "<p>ConnectionError</p>"
+    else:
+        return f"<p>Сейчас в космосе находиться " \
+               f"{answer.json()['number']} человек</p>"
 
 
 if __name__ == '__main__':
-    COSMONAUT_URL = "http://api.open-notify.org/astros.json"
-
     app.run(debug=True)
